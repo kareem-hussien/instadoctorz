fc466db8881ffa3af424836764a9114bcd03e439
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2024-06-25 01:16:14 +0300

    update

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 use Laracasts\Flash\Flash;
+use App\Models\Specialization;
 use App\Models\Setting;
 
 class RegisteredUserController extends Controller
@@ -25,8 +26,24 @@ class RegisteredUserController extends Controller
 
     public function doctorcreate(): View
     {
+        $prefixdata=[
+                        'Mr' => 'Mr','Mrs' => 'Mrs','Ms' => 'Ms','Mx' => 'Mx','Miss' => 'Miss','Dr' => 'Dr','Prof' => 'Prof'
+                    ];
+        $educationdata=[
+                        'Bachelor' => 'Bachelor',
+                        'Diploma' => 'Diploma',
+                        "Master's degree" => "Master's degree",
+                        'PHD' => 'PHD',
+                        'Other' => 'Other',
+                        ];
+
+        $specializations=Specialization::pluck('name', 'id')->toArray();
+        $experiencedata=['0-3'=>'0 - 3','4-6'=>'4 - 6','7-10'=>'7 - 10','10-15'=>'10 - 15','16+'=>'16+'];
+
+
+
         $captchakey = Setting::where('key','googleCaptchaKey')->pluck('value')->first();
-        return view('auth.doctoreregister',compact('captchakey'));
+        return view('auth.doctoreregister',compact('captchakey','prefixdata','educationdata','specializations','experiencedata'));
     }
 
     /**
